fe8c9273a0a6cccaa53275c44bf47a575d7675b6
erickl17 <36245779+erickl17@users.noreply.github.com>
2018-03-22 21:12:27 -0700

    Commit 2

--- Project_5/Project_5.ino
+++ Project_5/Project_5.ino
@@ -35,7 +35,7 @@ imu::Vector<3> euler;                         // Vector of IMU
 int distL = 0;                                // The distance left in degrees to desired heading
 int distR = 0;                                // The distance right in degrees to desired heading
 int steeringAngle = 0;                        // angle to turn the wheel to get to desired heading
-int carSpeed = 20;
+int carSpeed = 0;
 int distance = 0;
 
 void setup() {
@@ -48,19 +48,41 @@ void setup() {
     while (1);
   }
 
-  ///Setting the reference (Lat and Lon)///
+  byte c_data[22] = {255, 255, 0, 0, 250, 255, 205, 253, 175, 2, 16, 2, 1, 0, 254, 255, 1, 0, 232, 3, 27, 4}; // from P4
+  //byte c_data[22] = {0, 0, 0, 0, 0, 0, 211, 3, 117, 4, 55, 5, 255, 255, 255, 255, 255, 255, 232, 3, 21, 3};
+  bno.setCalibData(c_data);                                                                                       // SET CALIBRATION DATA
+  bno.setExtCrystalUse(true);
+
+  // GPS 
+  noInterrupts();           // disable all interrupts
+  TCCR4A = 0;
+  TCCR4B = 0;
+  TCNT4  = 336;             // every 1 second
+  TCCR4B |= (1 << CS42);    // 256 prescaler
+  TIMSK4 |= (1 << TOIE4);  // enable timer compare interrupt
+  interrupts();
+
+  GPS.begin(9600);
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   // 1 Hz update rate // it's more stable than 10Hz
+  GPS.sendCommand(PGCMD_ANTENNA);
+  useInterrupt(true);
+
+    ///Setting the reference (Lat and Lon)///
   localkey = 0;
   while (localkey != 1) {    // wait for select button
     lcd.clear();
     localkey = keypad.getKey();
     lcd.print("Press Select");
     lcd.setCursor(0, 1);
-    lcd.print("to save dest.");
+    lcd.print("to save dest."); 
     delay(100);               // delay to make display visible
   }
-  ReadGPS();
+  
+  ReadGPS(); 
   latDestination = lat;     // saving the destiantion point
   lonDestination = lon;     // saving the destiantion point
+  
   localkey = 0;
   while (localkey != 1) {   // wait for select button
     lcd.clear();
@@ -71,31 +93,14 @@ void setup() {
     delay(100);             // delay to make display visible
   }
 
-
-  byte c_data[22] = {0, 0, 0, 0, 0, 0, 211, 3, 117, 4, 55, 5, 255, 255, 255, 255, 255, 255, 232, 3, 21, 3};
-  bno.setCalibData(c_data);                                                                                       // SET CALIBRATION DATA
-  bno.setExtCrystalUse(true);
-  // set timer interrupts
+  // Steering and Actuate interupts
   noInterrupts();           // disable all interrupts
   TCCR1A = 0;
   TCCR1B = 0;
   TCNT1  = 59016;           // every 0.1 second
   TCCR1B |= (1 << CS12);    // 256 prescaler
   TIMSK1 |= (1 << TOIE1);  // enable timer compare interrupt
-
-  TCCR4A = 0;
-  TCCR4B = 0;
-  TCNT4  = 336;             // every 1 second
-  TCCR4B |= (1 << CS42);    // 256 prescaler
-  TIMSK4 |= (1 << TOIE4);  // enable timer compare interrupt
   interrupts();
-
-
-  GPS.begin(9600);
-  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
-  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   // 1 Hz update rate // it's more stable than 10Hz
-  GPS.sendCommand(PGCMD_ANTENNA);
-  useInterrupt(true);
 }
 
 SIGNAL(TIMER0_COMPA_vect) { // leave this function unchanged//
@@ -128,6 +133,11 @@ ISR(TIMER4_OVF_vect) { // This function will be called every 1 second
 
 void ReadGPS() {
   // read from GPS module and update the current position
+  Serial.println("ReadGPS() called");
+  if (GPS.newNMEAreceived()) {
+    if (!GPS.parse(GPS.lastNMEA()))   // this also sets the newNMEAreceived() flag to false
+      return;  // we can fail to parse a sentence in which case we should just wait for another
+  }
   lat = GPS.latitudeDegrees * 100000;
   lon = GPS.longitudeDegrees * 100000;
 }
@@ -141,6 +151,7 @@ void ReadHeading() { // Output: HEADING
 void CalculateBearing() {
   // calculate bearing angle based on current and destination locations (GPS coordinates)
   Bearing = atan2((latDestination - lat),(lonDestination - lon));
+  Bearing = Bearing * RAD_TO_DEG;
 }
 
 void CalculateSteering() { // Input: HEADING // Output: STEERANGLE
@@ -206,17 +217,19 @@ void CalculateSteering() { // Input: HEADING // Output: STEERANGLE
 
 void CalculateDistance() {
   // calculate distance to destination based on current and destination coordinates
-  distance = sqrt(pow((lonDestination - lon),2) + pow((latDestination - lat),2));
+  distance = sqrt(pow((lonDestination - lon),2) + pow((latDestination - lat),2));  
 }
 
 void Actuate() {
   // set car's direction and speed
-  if(distance <= 5) {
-    //int carSpeed = 0;
+  carSpeed = 20;
+  if(distance <= 1) {
+    int carSpeed = 0;
   }
 
-  analogWrite(44, steeringAngle);
-  myservo.write(carSpeed);
+  
+  analogWrite(carSpeedPin, carSpeed); //change to carSpeed for production
+  myservo.write(steeringAngle);
   
 }
 
@@ -233,31 +246,39 @@ ISR(TIMER1_OVF_vect) {        // This function will be called every 0.1 second
 
 void printHeadingOnLCD() {
   lcd.setCursor(0,0);
-  lcd.print("H: ");
-  lcd.print(euler.x() + 10.37);
+  lcd.print("H:");
+  float x = euler.x() + 10.37;
+  if(x > 360) {
+    x -= 360;
+  }
+  lcd.print(x);
 }
 
 void printLocationOnLCD() {
-  lcd.setCursor(0,1);
-  lcd.print("L: ");
+  lcd.setCursor(0,0);
+  lcd.print("LA:");
   lcd.print(lat);
-  lcd.print(", ");
+  lcd.setCursor(0,1);
+  lcd.print("LO:");
   lcd.print(lon);
 }
 
 void printDistanceOnLCD() {
-  lcd.setCursor(0,0);
-  lcd.print("D: ");
+  lcd.setCursor(0,1);
+  lcd.print("D:");
   lcd.print(distance);
+  lcd.setCursor(8,1);
+  lcd.print("B:");
+  lcd.print(Bearing);
 }
 
 void loop() {
   lcd.clear();    // clear the LCD
   // You can print anything on the LCD to debug your program!!!
   printHeadingOnLCD();
-  printLocationOnLCD();
-  delay(100);
-  lcd.clear();    // clear the LCD
   printDistanceOnLCD();
-  delay(500);
+  delay(1000);
+  lcd.clear();
+  printLocationOnLCD();
+  delay(1000);
 }
